398805f74dec0cc1839769fcebe5d39d6463c57d
Miguel Parada <miguelparada@gmail.com>
2023-06-21 14:56:22 -0500

    added http server

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+
+	"net/http"
 )
 
 type person struct {
@@ -12,24 +11,34 @@ type person struct {
 
 
 func main(){
-	p1 := person{
-		First: "Jenny",
-	}
-	p2 := person{
-		First: "James",
-	}
-	xp := []person{p1, p2}
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("Print JSON",string(bs))
+	// p1 := person{
+	// 	First: "Jenny",
+	// }
+	// p2 := person{
+	// 	First: "James",
+	// }
+	// xp := []person{p1, p2}
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("Print JSON",string(bs))
 
-	xp2 := []person{}
+	// xp2 := []person{}
 
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil{
-		log.Panic(err)
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil{
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("Back to a Go data structure", xp2)
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8080", nil)
 	}
-	fmt.Println("Back to a Go data structure", xp2)
-}
+	func foo(w http.ResponseWriter, r *http.Request){
+
+	}
+
+	func bar(w http.ResponseWriter, r *http.Request){
+
+	}
